484475bdc71ea46394c9de64b1392dde52fba075
bmwork <brenomartinsmb@gmail.com>
2023-09-10 11:42:31 -0300

    completed package.json logic with features

--- src/builder/mod.rs
+++ src/builder/mod.rs
@@ -1,10 +1,23 @@
 use crate::prompt::Inputs;
 use anyhow::{Context, Ok, Result};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::fs;
 use walkdir::WalkDir;
 
 const BASE_DIRECTORY: &str = "templates/base/";
-const FEATURES_DIRECTORY: &str = "templates/features/";
+const BASE_FEATURES_DIRECTORY: &str = "templates/features/";
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PackageJson {
+    name: Option<String>,
+    version: Option<String>,
+    r#type: Option<String>,
+    scripts: Option<HashMap<String, Option<String>>>,
+    dependencies: Option<HashMap<String, Option<String>>>,
+    dev_dependencies: Option<HashMap<String, Option<String>>>,
+}
 
 pub fn create(inputs: Inputs) -> Result<()> {
     create_dir(&inputs)?;
@@ -16,8 +29,7 @@ pub fn create(inputs: Inputs) -> Result<()> {
 
 pub fn create_dir(inputs: &Inputs) -> Result<()> {
     if inputs.directory.as_path().exists() {
-        println!("Directory already exists, please change the project name.");
-        std::process::exit(0);
+        fs::remove_dir_all(&inputs.directory)?;
     }
 
     fs::create_dir(&inputs.directory).context("Could not create directory to project.")?;
@@ -30,9 +42,7 @@ pub fn build(inputs: Inputs) -> Result<()> {
 
         let source_path = entry.path();
         let relative_path = source_path.strip_prefix(&BASE_DIRECTORY).unwrap();
-        let destination_path = fs::canonicalize(&inputs.directory)
-            .unwrap_or_default()
-            .join(relative_path);
+        let destination_path = fs::canonicalize(&inputs.directory).unwrap_or_default().join(relative_path);
 
         if entry.file_type().is_file() {
             fs::copy(&source_path, &destination_path)?;
@@ -42,28 +52,63 @@ pub fn build(inputs: Inputs) -> Result<()> {
     }
 
     if !inputs.features.is_empty() {
-        for feature in inputs.features.iter() {
-            for entry in WalkDir::new(&FEATURES_DIRECTORY) {
-                let entry = entry?;
-                if entry.file_name().to_str().unwrap().to_string() == feature.to_lowercase() {
-                    println!(
-                        "{} = {}: {}",
-                        entry.file_name().to_str().unwrap().to_string(),
-                        feature.to_lowercase(),
-                        entry.file_name().to_str().unwrap().to_string() == feature.to_lowercase()
-                    );
-                    println!("{:?}", entry.file_name());
-                    let source_path = entry.path();
-                    let relative_path = source_path.strip_prefix(&FEATURES_DIRECTORY).unwrap();
-                    let destination_path = fs::canonicalize(&inputs.directory)
-                        .unwrap_or_default()
-                        .join(relative_path);
+        let project_package_json_path = format!("{}/{}", &inputs.directory.to_str().unwrap(), "package.json");
+        let project_content = fs::read_to_string(&project_package_json_path).unwrap();
+        let mut project_package_json: PackageJson = serde_json::from_str(&project_content).unwrap();
+        let mut project_dependencies: HashMap<String, Option<String>> = HashMap::new();
+        let mut project_dev_dependencies: HashMap<String, Option<String>> = HashMap::new();
 
-                    if entry.file_type().is_file() {
+        for feature in inputs.features.iter() {
+            let feature_directory = format!("{}{}", BASE_FEATURES_DIRECTORY, feature);
+            for entry in WalkDir::new(&feature_directory) {
+                let entry = entry?;
+
+                let source_path = entry.path();
+                let relative_path = source_path.strip_prefix(&feature_directory).unwrap();
+                let destination_path = fs::canonicalize(&inputs.directory).unwrap_or_default().join(relative_path);
+
+                if entry.file_type().is_file() {
+                    let sufix = entry.file_name().to_str().unwrap().split(".").collect::<Vec<&str>>()[1];
+
+                    if sufix == "json" {
+                        let project_name = inputs.directory.to_str().unwrap().split("./").collect::<Vec<&str>>()[1];
+                        project_package_json.name = Some(project_name.to_string());
+                        let feature_package_json_path = format!("{}/{}", &feature_directory, entry.file_name().to_str().unwrap());
+                        let feature_content = fs::read_to_string(feature_package_json_path).unwrap();
+                        let feature_package_json: PackageJson = serde_json::from_str(&feature_content).unwrap();
+
+                        if feature_package_json.dependencies.is_some() {
+                            let dependencies = feature_package_json.dependencies.unwrap();
+                            for (key, value) in dependencies.iter() {
+                                project_dependencies.insert(key.to_owned(), value.to_owned());
+                            }
+                            if let Some(project_deps) = project_package_json.dependencies {
+                                for (key, value) in project_deps.iter() {
+                                    project_dependencies.insert(key.to_owned(), value.to_owned());
+                                }
+                            }
+                            project_package_json.dependencies = Some(project_dependencies.clone());
+                        }
+
+                        if feature_package_json.dev_dependencies.is_some() {
+                            let dev_dependencies = feature_package_json.dev_dependencies.unwrap();
+                            for (key, value) in dev_dependencies.iter() {
+                                project_dev_dependencies.insert(key.to_owned(), value.to_owned());
+                            }
+                            if let Some(project_deps) = project_package_json.dev_dependencies {
+                                for (key, value) in project_deps.iter() {
+                                    project_dev_dependencies.insert(key.to_owned(), value.to_owned());
+                                }
+                            }
+                            project_package_json.dev_dependencies = Some(project_dev_dependencies.clone());
+                        }
+
+                        fs::write(&project_package_json_path, serde_json::to_string_pretty::<PackageJson>(&project_package_json).unwrap())?;
+                    } else {
                         fs::copy(&source_path, &destination_path)?;
-                    } else if entry.file_type().is_dir() {
-                        fs::create_dir_all(&destination_path)?;
                     }
+                } else if entry.file_type().is_dir() {
+                    fs::create_dir_all(&destination_path)?;
                 }
             }
         }
